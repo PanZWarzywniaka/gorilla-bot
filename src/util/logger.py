22f25114df317273ed148c9cbcc99c08f165af3c
Aleksander Osikowicz <tupolewcx@gmail.com>
2022-06-28 13:47:00 +0100

    Feat(logger): new format

--- src/util/logger.py
+++ src/util/logger.py
@@ -2,7 +2,7 @@ import logging
 # from discord_logger import DiscordLogger
 
 logging.basicConfig(filename='gorilla-bot.log', level=logging.INFO,
-                    format='%(asctime)s [%(levelname)s] %(message)s')
+                    format='%(asctime)s %(levelname)s %(message)s')
 
 
 def log_info(message: str):
